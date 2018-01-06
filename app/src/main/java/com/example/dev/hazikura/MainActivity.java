c0a628d8b7af97f292154b8f60448b019622ca64
yokoro13 <yokoro1219@yahoo.co.jp>
2018-01-06 22:42:17 +0900

    menu追加 TO DO fragmentを用いた画面遷移

--- app/src/main/java/com/example/dev/hazikura/MainActivity.java
+++ app/src/main/java/com/example/dev/hazikura/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.dev.hazikura;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +20,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.household_accounts:
+                    mTextMessage.setText(R.string.title_household_accounts);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.b:
+                    mTextMessage.setText(R.string.title_b);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.c:
+                    mTextMessage.setText(R.string.title_c);
+                    return true;
+                case R.id.d:
+                    mTextMessage.setText(R.string.title_d);
+                    return true;
+                case R.id.e:
+                    mTextMessage.setText(R.string.title_option);
                     return true;
             }
             return false;
@@ -39,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), OptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
